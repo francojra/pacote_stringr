04e4481f19d7810d2016c2c8f50461bae424496b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-02-06 21:47:58 -0300

    Add files via upload

--- pacote_stringr.R
+++ pacote_stringr.R
@@ -12,4 +12,40 @@ library(stringr)
 
 # Funções básicas --------------------------------------------------------------------------------------------------------------------------
 
+str_length("São Paulo")
+## [1] 9
 
+str_length(c("São Paulo", "Rio de Janeiro", 
+             "Rio Grande do Norte", "Acre"))
+## [1]  9 14 19  4
+
+s <- c("São Paulo", "Rio de Janeiro", 
+       "Rio Grande do Norte", "Acre")
+
+str_length(s)
+## [1]  9 14 19  4
+
+length(s)
+## [1] 4
+
+s <- "Somos a curso-r"
+
+str_to_lower(s)
+## [1] "somos a curso-r"
+
+str_to_upper(s)
+## [1] "SOMOS A CURSO-R"
+
+str_to_title(s)
+## [1] "Somos A Curso-R"
+
+s <- "Somos a curso-r"
+
+str_to_lower(s)
+## [1] "somos a curso-r"
+
+str_to_upper(s)
+## [1] "SOMOS A CURSO-R"
+
+str_to_title(s)
+## [1] "Somos A Curso-R"
